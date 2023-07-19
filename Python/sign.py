f08c51a6783a3e9654100615c852f2b8879f7875
Geoffrey J. Teale <geoffrey@upvest.co>
2023-07-19 11:00:37 +0200

    Improve formatting in sign.py

--- Python/sign.py
+++ Python/sign.py
@@ -11,10 +11,10 @@ import sys
 import upvest.http as up_http
     
 
-
 if __name__ == "__main__":
     if len(sys.argv) != 6:
-        print("sign.py <PEM FILE> <PEM password> <Preshared Key ID> <Client ID> <Client Secret>\n")
+        print("sign.py <PEM FILE> <PEM password> <Preshared Key ID> "
+              "<Client ID> <Client Secret>\n")
         sys.exit(1)
 
     pem_file = sys.argv[1]
@@ -23,7 +23,9 @@ if __name__ == "__main__":
     client_id = sys.argv[4]
     client_secret = sys.argv[5]
     
-    api = up_http.UpvestAPI("sandbox.upvest.co", pem_file, pem_password, preshared_key_id, client_id, client_secret, scopes=["users:admin"])
+    api = up_http.UpvestAPI("sandbox.upvest.co", pem_file, pem_password,
+                            preshared_key_id, client_id, client_secret,
+                            scopes=["users:admin"])
     resp = api.get("/users", params={"limit": 2})
     
     print(resp.json())
